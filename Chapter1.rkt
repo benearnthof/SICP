a25cdf247fedb8bf9c904b621fe9bbc1770b5dd5
unknown <benearnthof@hotmail.de>
2021-02-14 18:30:35 +0100

    continue to 1.7

--- Chapter1.rkt
+++ Chapter1.rkt
@@ -1,4 +1,5 @@
 #lang sicp
+; TODO: Learn DrRacket hotkeys
 ; Elements of Programming: Combining primitive expressions to keep abstracting.
 ; Lisp primitives:
 (+ 1 4)
@@ -75,6 +76,71 @@
 (define (<= x y)
   (not (> x y)))
 ; TODO: Exercises
-; TODO: Fix stream setup => Audio issues.
-; TODO: Add mic and webcam
-; TODO: Learn DrRacket hotkeys
+(+ 5 3 4)
+(- 9 1)
+(/ 6 2)
+(+ (* 2 4)(- 4 6))
+(define a 3)
+(define b (+ a 1))
+(+ a b (* a b))
+(= a b)
+(if (and (> b a) (< (* a b)))
+    b
+    a)
+; returns b
+(+ 2 (if (> b a) b a)) ; returns 6
+(cond ((= a 4) 6)
+      ((= b 4) (+ 6 7 a))
+      (else 25)) ; should return 16
+(+ 2 (if (> b a) b a)) ; should return 6
+
+(* (cond ((> a b) a)
+   ((< a b) b)
+   (else -1))
+( + a 1)) ; 4 * 4 evaluates to 16
+; 1.2 Translate into prefix form:
+(/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7))) ; seems correct
+; 1.3 Define a procedure that takes three numbers as arguments and returns the
+; sum of the squares of the two larger numbers
+(define (ssqlarge x y z)
+  (cond ((and (< x y) (< x z)) (sumofsquares z y))
+        ((and (< y x) (< y z)) (sumofsquares x z))
+        ((and (< z y) (< z x)) (sumofsquares x y))))
+
+; 1.4 Observe that our model of evaluation allows for combinations whose
+; operators are compound expressions. Use this observation to describe the
+; behavior of the following procedure
+(define (a_plus_abs_b a b)
+  ((if (> b 0) + -) a b))
+; the operator to combine a and b is itself a compound expression
+; this is really slick, im not gonna lie
+
+; 1.5 Applicative vs normal order evaluation
+;(define (p) (p))
+(define (test x y)
+  (if (= x 0) 0 y))
+; applicative order evaluation will unnest first, thus the test in the book
+; should just crash since evaluating p just returns p in an endless loop
+; normal order evaluation should just return 0
+;(test 0 (p))
+
+; Newtons method for square roots
+(define (sqrtiter guess x)
+  (if (goodenough? guess x)
+      guess
+      (sqrtiter (improve guess x) x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (goodenough? guess x)
+  (< (abs (- (square guess) x)) 0.000001))
+
+; recursion allows us to avoid loops
+(define (sqrt x)
+  (sqrtiter 1 x))
+
+; 1.7
